857b44290197a3926c432265c2d4d4d9d364f9ed
kit <kit@DESKTOP-EHKDJE1>
2025-07-05 23:08:59 +0300

    String_SPU_411_LIB_DONE

--- String_Check/main.cpp
+++ String_Check/main.cpp
@@ -2,10 +2,18 @@
 
 void main()
 {
-	setlocale (LC_ALL, " ");
+	setlocale(LC_ALL, "");
 
-	String str1 = "Hello";
+
+
+   String str1 = "Hello";
 	cout << str1 << endl;
+	String str2 = "World";
+	cout << str2 << endl;
+	cout << delimiter << endl;
+	String str3 = str1 + str2;
+	cout << str3 << endl;
+	cout << delimiter << endl;
 
 
 }
